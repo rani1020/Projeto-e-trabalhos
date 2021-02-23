1226489de4b24f3d347768c7b08ebcde0c14f9d9
rani1020 <79539287+rani1020@users.noreply.github.com>
2021-02-23 16:24:15 -0300

    Codigo site PROJETO FACULDADE LOJA VIRTUAL

---
+++ site_dan_store.js
@@ -0,0 +1,12 @@
+//abrir uma nova janela ao clicar no link
+var  janela
+var  janela2
+
+function abrirjanela() {
+    janela = window.open("https://chat.whatsapp.com/JzcfoNadTVqK35gf19YEQw", "nova_janela" )
+    
+}
+
+function abrirjanela2() {
+    janela2 = window.open("https://www.instagram.com/dan_store__/?hl=pt-br", "nova_janela2")
+}
